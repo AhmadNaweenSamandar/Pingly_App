d4a9325af305854a28a7aa993c03d6777df9140b
Ahmad Naween Samandar <asama017@uottawa.ca>
2025-12-23 22:04:48 -0500

    Add: constant keywords added for hobbies, personalitytypes and lookingfor, these keywords will help users to match with eachother

--- src/app/components/Modals/ProfileModal.tsx
+++ src/app/components/Modals/ProfileModal.tsx
@@ -13,3 +13,27 @@ interface ProfileModalProps {
   onClose: () => void;
 }
 
+/* Specific constant keywords added for matching criteria
+   this will be used to define matching criteria for user
+   they will help users match based on common constants
+   */
+const technicalSkills = [
+  "JavaScript", "Python", "Java", "C++", "React", "Node.js", 
+  "TypeScript", "SQL", "MongoDB", "Docker", "AWS", "Git",
+  "Machine Learning", "Data Science", "UI/UX Design", "Mobile Development"
+];
+
+const hobbies = [
+  "Reading", "Gaming", "Sports", "Music", "Art", "Cooking",
+  "Photography", "Traveling", "Hiking", "Dancing", "Writing", "Coding"
+];
+
+const personalityTypes = [
+  "INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP",
+  "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP"
+];
+
+const lookingFor = [
+  "Study Partner", "Project Collaborator", "Mentor", "Friend",
+  "Coffee Buddy", "Gym Partner", "Dating", "Networking"
+];
