98d632b5448da39b312856ca0256a9789ed49f0e
Ahmad Naween Samandar <asama017@uottawa.ca>
2025-12-24 19:47:02 -0500

    Add: constants added for various components of profile section, user can select many for one attribute

--- src/app/components/Auth/RegistrationForm.tsx
+++ src/app/components/Auth/RegistrationForm.tsx
@@ -12,3 +12,27 @@ interface RegistrationFormProps {
   onComplete: () => void;
 }
 
+//constant created for selection while filling the reg form
+//these forms will be used in matching algorithm
+//user will be recommended based on these constants
+const technicalSkills = [
+  "JavaScript", "Python", "Java", "C++", "React", "Node.js", 
+  "TypeScript", "SQL", "MongoDB", "Docker", "AWS", "Git",
+  "Machine Learning", "Data Science", "UI/UX Design", "Mobile Development"
+];
+
+const hobbies = [
+  "Reading", "Gaming", "Sports", "Music", "Art", "Cooking",
+  "Photography", "Traveling", "Hiking", "Dancing", "Writing", "Coding"
+];
+
+const personalityTypes = [
+  "INTJ", "INTP", "ENTJ", "ENTP", "INFJ", "INFP", "ENFJ", "ENFP",
+  "ISTJ", "ISFJ", "ESTJ", "ESFJ", "ISTP", "ISFP", "ESTP", "ESFP"
+];
+
+const lookingFor = [
+  "Study Partner", "Project Collaborator", "Mentor", "Friend",
+  "Coffee Buddy", "Gym Partner", "Dating", "Networking"
+];
+
