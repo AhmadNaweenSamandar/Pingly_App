f6d92bbe287719982cc49699cf83f7f345fefe83
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-03-17 00:16:38 -0400

    ADD: prisma global module wired with app module to make it availabel through out the app

--- Packages/server/src/app.module.ts
+++ Packages/server/src/app.module.ts
@@ -3,11 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './modules/auth/auth.module';
 import { UsersModule } from './modules/users/users.module';
-import { ProfessionalModule } from './modules/professional/professional.module';
-import { SocialModule } from './modules/social/social.module';
+
+//prisma module import 
+import { PrismaModule } from 'prisma/prisma.module';
+
+/* we need to wire this into the main entry point of our backend so NestJS knows it exists and it can be availabe in whole app */
 
 @Module({
-  imports: [AuthModule, UsersModule, ProfessionalModule, SocialModule],
+  imports: [AuthModule, UsersModule, PrismaModule], // Importing the PrismaModule here makes the PrismaService available throughout the app
+  /* Because we used the @Global() decorator, we will never need to import PrismaModule into your ProjectsModule or UsersModule.
+  When we start writing our business logic, we simply inject it into the constructor of any service, like this:
+  constructor(private readonly prisma: PrismaService) {}
+  Then use it: await this.prisma.user.findMany(); */
   controllers: [AppController],
   providers: [AppService],
 })
