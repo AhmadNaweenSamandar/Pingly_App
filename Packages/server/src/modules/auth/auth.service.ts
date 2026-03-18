d52b83ea5fd983e3bb251e90a68c4654a14c6527
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-03-17 23:55:29 -0400

    ADD: detailed comments in auth service for documentation

--- Packages/server/src/modules/auth/auth.service.ts
+++ Packages/server/src/modules/auth/auth.service.ts
@@ -3,7 +3,12 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'prisma/prisma.service'; 
 
+/**
+ * @Injectable() means this class can be "injected" into other files (like our controller).
+ */
 @Injectable()
+
+// We need Prisma to talk to the database, and JwtService to create tokens.
 export class AuthService {
   constructor(
     private prisma: PrismaService,
@@ -11,16 +16,27 @@ export class AuthService {
   ) {}
 
   async devLogin(email: string, name: string) {
-    // 1. The Backend Bouncer: Check if the email is from the allowed domain
+
+    // ---------------------------------------------------------
+    // RULE 1: THE DOMAIN CHECK
+    // ---------------------------------------------------------
+    // We immediately reject anyone who doesn't belong to uottawa.ca.
+    // This protects our app from outsiders.
+    // later we will implement Microsoft SSO entra ID auth in accordance with uottawa IT team requirements 
     if (!email.endsWith('@uottawa.ca')) {
-      throw new UnauthorizedException('Only company emails are allowed.');
+      throw new UnauthorizedException('Only uottawa.ca emails are allowed.');
     }
 
-    // 2. Database Check: Find or Create the user
+    // ---------------------------------------------------------
+    // RULE 2: FIND OR CREATE THE USER
+    // ---------------------------------------------------------
+    // We ask Prisma: "Does a user with this email already exist?"
     let user = await this.prisma.user.findUnique({
       where: { email },
     });
 
+
+    // If they do not exist, this is their first time logging in. We must create them.
     if (!user) {
       user = await this.prisma.user.create({
         data: {
@@ -32,13 +48,21 @@ export class AuthService {
       console.log(`Created new dev user: ${email} with name: ${name}`);
     }
 
-    // 3. Generate the JWT Payload
+    // ---------------------------------------------------------
+    // RULE 3: MINT THE JSON WEB TOKEN (JWT)
+    // ---------------------------------------------------------
+    // The payload is the public information hidden inside the token.
+    // 'sub' (Subject) is the industry-standard term for the User ID.
     const payload = { sub: user.id, email: user.email };
 
-    // 4. Return the Token and User Data to React
+    // We sign the token using our secret key. React will save this token.
     return {
       access_token: this.jwtService.sign(payload),
-      user: user, 
-    };
+      user: {
+        id: user.id,
+        email: user.email,
+        name: user.name
+      },
   }
+}
 }
