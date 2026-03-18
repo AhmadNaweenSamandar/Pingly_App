a3849425c13c8d876fd1d96bbec9960cdf405684
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-03-18 00:05:14 -0400

    ADD: cors enabled and localhost of front end added to backend

--- Packages/server/src/main.ts
+++ Packages/server/src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Allows our React app to make requests to this backend
+  app.enableCors();
+
+  // our front end localhost
+  await app.listen(5173);
 }
 bootstrap();
