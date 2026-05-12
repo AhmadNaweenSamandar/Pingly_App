35f488a5b78efa16d4f821b2afd7f5833d9965df
Ahmad Naween Samandar <asama017@uottawa.ca>
2026-05-11 21:58:17 -0400

    ADD: skills for join request added

--- Packages/client/src/app/components/ProjectIdeaCard.tsx
+++ Packages/client/src/app/components/ProjectIdeaCard.tsx
@@ -31,6 +31,15 @@ interface ProjectIdeaProps {
 }
 
 
+// skills list for the project idea join request (can be used for a dropdown)
+const professionalSkills = [
+  "JavaScript", "Python", "Java", "C++", "React", "Node.js", 
+  "TypeScript", "SQL", "MongoDB", "Docker", "AWS", "Git",
+  "Machine Learning", "Data Science", "Mobile Development",
+  "UI/UX Design", "Graphic Design", "Video Editing", "Copywriting", "3D Modeling",
+  "Project Management", "Financial Modeling", "Marketing", "Sales", "Public Speaking", "Data Analysis"
+];
+
 /**
  * ProjectIdeaCard Component
  * * Displays a single project concept/pitch card.
